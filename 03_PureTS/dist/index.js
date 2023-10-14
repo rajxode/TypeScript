1f2ee6db09d24060e69f18b835ff7b2336abec54
Rajeev Kumar Sharma <120395470+rajxode@users.noreply.github.com>
2023-10-14 19:01:05 +0000

    Classes and modifiers in TS

--- 03_PureTS/dist/index.js
+++ 03_PureTS/dist/index.js
@@ -1,2 +1,21 @@
 "use strict";
-console.log('TypeScript is running ');
+// classes in TS
+class User {
+    // initialize the variables using constructor
+    constructor(name, email) {
+        this.city = 'Delhi';
+        this.name = name;
+        this.email = email;
+    }
+}
+// another way of declaring class in TS
+class Person {
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+        this.city = 'Delhi';
+    }
+}
+// creating new user from class 
+const newUser = new User('myname', 'abc@xyz.com');
+const newPerson = new Person('personName', 'person@xyz.com');
--- 03_PureTS/src/index.ts
+++ 03_PureTS/src/index.ts
@@ -1,1 +1,32 @@
-console.log('TypeScript is running ')
+
+// classes in TS
+class User {
+    // define type of variables in class
+    // public modifier , can be accessed outside of the class
+    public name:string;
+    // private can't be accessed outside of the class , JS equivalent '#email'
+    private email:string;
+    readonly city: string = 'Delhi';
+
+    // initialize the variables using constructor
+    constructor(name:string,email:string){
+        this.name = name;
+        this.email = email;
+    }
+}
+
+
+// another way of declaring class in TS
+class Person {
+    readonly city:string = 'Delhi'
+    constructor(
+        public name:string,
+        public email:string
+    ){
+    }
+}
+
+
+// creating new user from class 
+const newUser = new User ('myname','abc@xyz.com');
+const newPerson = new Person('personName','person@xyz.com');
