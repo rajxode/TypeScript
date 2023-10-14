a404584896a2e2e39c99daaeb8648a49ac4011ba
Rajeev Kumar Sharma <120395470+rajxode@users.noreply.github.com>
2023-10-14 19:15:50 +0000

    Getters , Setters and Protected Modifier

--- 03_PureTS/dist/index.js
+++ 03_PureTS/dist/index.js
@@ -10,10 +10,40 @@ class User {
 }
 // another way of declaring class in TS
 class Person {
+    // constructor to assign values
     constructor(name, email) {
         this.name = name;
         this.email = email;
+        // private variable  cannot be accessed outside of the class
+        this.count = 1;
+        // protected variable only accessable inside this class and subclass
+        this.num = 5;
         this.city = 'Delhi';
+    }
+    // private function 
+    reduceCount() {
+        console.log('count reduce');
+    }
+    // a getter function to get the count value outside the class
+    get getCount() {
+        return this.count;
+    }
+    // a setter function to set the value of count variable
+    set setCount(num) {
+        if (num < 1) {
+            throw new Error("Value cannot be less than 1");
+        }
+        this.count += num;
+    }
+}
+class Children extends Person {
+    constructor() {
+        super(...arguments);
+        this.age = 5;
+    }
+    // change the protected variable of parent class
+    changeNum() {
+        this.num = 15;
     }
 }
 // creating new user from class 
--- 03_PureTS/src/index.ts
+++ 03_PureTS/src/index.ts
@@ -18,11 +18,49 @@ class User {
 
 // another way of declaring class in TS
 class Person {
+
+    // private variable  cannot be accessed outside of the class
+    private count : number = 1;
+
+    // protected variable only accessable inside this class and subclass
+    protected num : number = 5;
+
     readonly city:string = 'Delhi'
+    // constructor to assign values
     constructor(
         public name:string,
         public email:string
     ){
+    }
+
+    // private function 
+    private reduceCount():void{
+        console.log('count reduce');
+    }
+
+
+    // a getter function to get the count value outside the class
+    get getCount() : number {
+        return this.count;
+    }
+
+    // a setter function to set the value of count variable
+    set setCount(num : number){
+        if(num < 1 ){
+            throw new Error("Value cannot be less than 1");
+            
+        }
+        this.count += num;
+    }
+}
+
+
+class Children extends Person{
+    public age:number = 5;
+
+    // change the protected variable of parent class
+    changeNum(){
+        this.num = 15;
     }
 }
 
